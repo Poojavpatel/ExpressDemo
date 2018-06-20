3458a131d759566abd336af1031e4f2ac0f030f3
Pooja patel <poojavpatel71@gmail.com>
2018-06-20 23:40:34 +0530

    Handling http get request done

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.get('/api/courses/' , (req , res) => {
     res.send(courses);
 });
 
-app.get('/api/courses/:id' , (req , res) => {
-    res.send(req.params);
-});
+// app.get('/api/courses/:id' , (req , res) => {
+//     res.send(req.params);
+// });
 
 app.get('/api/courses/:year/:month' , (req , res) => {
     res.send(req.params);
@@ -28,6 +28,17 @@ app.get('/api/courses/:year/:month' , (req , res) => {
 //Query string parameters
 app.get('/api/myquestion' , (req , res) => {
     res.send(req.query);
+});
+
+//handling HTTP GET requests
+app.get('/api/courses/:id' , (req , res) => {
+    let a = parseInt(req.params.id);
+    const course = courses.find(c => c.id === a);
+   // if course not found send a 404
+    if(!course){
+        res.status(404).send("the course was not found");
+    }
+    res.send(course);
 });
 
 //Environment variable
